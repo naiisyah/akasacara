5cf5a5cd7de45ac205142ef30c4a0194f46ce6f1
naiisyah <syahlanaiimah19@gmail.com>
2025-09-23 17:28:06 +0700

    Feat : Slideshow, aboute use, & our work -Syahla

--- src/app/beranda/page.tsx
+++ src/app/beranda/page.tsx
@@ -4,7 +4,23 @@ const Beranda = () => {
   return (
     <div className="bg-white text-black min-h-screen font-sans">
       {/* START SHOWREEL */}
-      <section className="showreel"></section>
+      <section className="showreel relative w-screen h-screen overflow-hidden">
+        <video 
+          src="/assets/showreel.mp4" 
+          autoPlay 
+          loop 
+          playsInline 
+          className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+
+        <div className="absolute top-6 left-6 z-10">
+          <img 
+            src="/assets/logo.png" 
+            alt="Akasacara Film Logo" 
+            className="w-50 h-auto"
+          />
+        </div>
+      </section>
       {/* END SHOWREEL */}
 
       {/* START ANNOUNCEMENT */}
@@ -39,11 +55,11 @@ const Beranda = () => {
       {/* END OUR WORK */}
 
       {/* START ABOUTE US */}
-      <section className="bg-white-100 p-6 mb-8 px-20">
-        <h2 className="font-plus-jakarta-sans text-heading-1 text-black mb-4">ABOUT US!</h2>
-        <div className="flex flex-col md:flex-row gap-6 items-center">
+      <section className="g-white-100 pt-24 pr-[78px] pb-[78px] pl-[78px] mb-8">
+        <h2 className="font-plus-jakarta-sans text-6xl text-black mb-12">About Us!</h2>
+        <div className="flex flex-col md:flex-row items-center md:space-x-8">
           <img src="/assets/aboutus.jpg" alt="About Us Akasacara" className="w-full md:w-1/2" />
-          <p className="text-base text-black-500 leading-relaxed">
+          <p className="font-rubik text-3xl text-black">
             Akasacara Film is a small independent multimedia company based in Yogyakarta, Indonesia. Akasacara Film has a role to provide creative innovation in multimedia.
             Akasacara Film produced a film and distributed it to film festivals and cinemas. The creations which have been produced by Akasacara Film include narrative films, 
           </p>
